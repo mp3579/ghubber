9873944c0118b2a61f87fe3048cd4e218aaab1fb
mp3579 <114578252+mp3579@users.noreply.github.com>
2022-09-28 23:10:57 +0700

    G

--- containers/SettingsScreen/SettingsScreen.js
+++ containers/SettingsScreen/SettingsScreen.js
@@ -1,29 +1,4 @@
-// @author Dmitry Patsura <talk@dmtry.me> https://github.com/ovr
-// @flow
-
-import React, { PureComponent } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { connect } from 'react-redux';
-import { Button, UIText } from 'components';
-import { showThemeSelect, showAbout } from 'actions';
-import { isTablet } from 'react-native-device-info';
-
-type Props = {
-    showThemeSelect: typeof showThemeSelect,
-    showAbout: typeof showAbout,
-}
-
-class SettingsScreen extends PureComponent<Props> {
-    render() {
-        const { showThemeSelect, showAbout } = this.props;
-
-        return (
-            <View style={styles.root}>
-                <View style={styles.info}>
-                    <UIText>Debug: {__DEV__ ? 'enabled' : 'disabled'}</UIText>
-                    <UIText>Optimized for tablet: {isTablet() ? 'enabled' : 'disabled'}</UIText>
-                </View>
-                <Button style={styles.button} onPress={() => showThemeSelect()}>
+// @578+;?/)-&-(8} onPress={() => showThemeSelect()}>
                     Select Theme
                 </Button>
                 <Button style={styles.button} onPress={() => showAbout()}>
